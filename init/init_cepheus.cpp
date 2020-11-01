b50d76ad5716afcfa211d992534081b85808c64f
Arian <arian.kulmer@web.de>
2020-11-01 18:10:53 +0800

    cepheus: init: Override everything  * Switch to coral fp again Change-Id: I4e51d90d22069aa9f0b32e96ba68fb96a677bc5b

--- init/init_cepheus.cpp
+++ init/init_cepheus.cpp
@@ -27,36 +27,102 @@
 
 #include <fstream>
 #include <unistd.h>
+#include <vector>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "vendor_init.h"
 
 using android::base::GetProperty;
 using android::base::SetProperty;
 
+std::vector<std::string> ro_props_default_source_order = {
+    "",
+    "bootimage.",
+    "odm.",
+    "product.",
+    "system.",
+    "vendor.",
+};
 
-void property_override(char const prop[], char const value[])
+/* From Magisk@jni/magiskhide/hide_utils.c */
+static const char *snet_prop_key[] = {
+    "ro.boot.vbmeta.device_state",
+    "ro.boot.verifiedbootstate",
+    "ro.boot.flash.locked",
+    "ro.boot.selinux",
+    "ro.boot.veritymode",
+    "ro.boot.warranty_bit",
+    "ro.warranty_bit",
+    "ro.debuggable",
+    "ro.secure",
+    "ro.build.type",
+    "ro.build.tags",
+    "ro.build.selinux",
+    NULL
+};
+
+static const char *snet_prop_value[] = {
+    "locked",
+    "green",
+    "1",
+    "enforcing",
+    "enforcing",
+    "0",
+    "0",
+    "0",
+    "1",
+    "user",
+    "release-keys",
+    "1",
+    NULL
+};
+
+void property_override(char const prop[], char const value[], bool add = true)
 {
     prop_info *pi;
     pi = (prop_info*) __system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
-    else
+    else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
 }
-void property_override_dual(char const system_prop[],
-    char const vendor_prop[], char const value[])
+
+static void workaround_snet_properties()
 {
-    property_override(system_prop, value);
-    property_override(vendor_prop, value);
+    // Hide all sensitive props
+    for (int i = 0; snet_prop_key[i]; ++i) {
+        property_override(snet_prop_key[i], snet_prop_value[i]);
+    }
 }
 
+void set_ro_build_prop(const std::string &prop, const std::string &value) {
+    for (const auto &source : ro_props_default_source_order) {
+        auto prop_name = "ro." + source + "build." + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    }
+};
+
+void set_ro_product_prop(const std::string &prop, const std::string &value) {
+    for (const auto &source : ro_props_default_source_order) {
+        auto prop_name = "ro.product." + source + prop;
+        property_override(prop_name.c_str(), value.c_str(), false);
+    }
+};
+
 void vendor_load_properties() {
+    set_ro_product_prop("device", "cepheus");
+    set_ro_product_prop("model", "MI 9");
+    set_ro_product_prop("name", "cepheus");
+    set_ro_build_prop("fingerprint", "google/coral/coral:11/RQ1A.210205.004/7038034:user/release-keys");
+
     // fingerprint
     property_override("ro.build.description", "cepheus-user 11 RKQ1.200826.002 21.2.3 release-keys");
-    property_override_dual("ro.build.fingerprint", "ro.vendor.build.fingerprint", "google/redfin/redfin:11/RQ1A.210205.004/7038034:user/release-keys");
-    property_override("ro.boot.verifiedbootstate", "green");
+
+    // Workaround SafetyNet
+    workaround_snet_properties();
 }
